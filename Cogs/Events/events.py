1c622c188f25968a5df3f861cb7c2a1204a0ba2a
SapphireRP31 <SapphireRP31@proton.me>
2023-05-16 22:30:42 -0400

    Suffering from the Effects of Events.py

--- Cogs/Events/events.py
+++ Cogs/Events/events.py
@@ -5,6 +5,7 @@ class EventLogging(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    # Message Edit
     @commands.Cog.listener()
     async def on_message_update(self, event: guilded.MessageUpdateEvent):
         before = event.before
@@ -18,12 +19,13 @@ class EventLogging(commands.Cog):
             channel = await guild.fetch_channel('790f5bb8-8f1c-4fa5-9ed8-518ebacf50a0')
             if channel:
                 embed = guilded.Embed()
-                embed.title  = "A message has been edited:"
-                embed.description = f"{before.author.name} edited a message in #{before.channel.name}:"
+                embed.title  = "A Message Has Been Edited:"
+                embed.description = f"{before.author.name} Edited A Message In #{before.channel.name}:"
                 embed.add_field(name="Before", value=f"`{before.content}`")
                 embed.add_field(name="After", value=f"`{after.content}`")
                 await channel.send(embed=embed)
 
+    # Nickname Change
     @commands.Cog.listener()
     async def on_member_update(self, event: guilded.MemberUpdateEvent):
         before = event.before
@@ -35,9 +37,9 @@ class EventLogging(commands.Cog):
             if channel:
                 embed = guilded.Embed()
                 if after.nick is not None:
-                    embed.title = f"{before.name} changed their nickname:"
+                    embed.title = f"{before.name} Changed Their Nickname:"
                 else:
-                    embed.title = f"{before.name} resetted their nickname:"
+                    embed.title = f"{before.name} Resetted Their Nickname:"
                 if before.nick is not None:
                     embed.add_field(name="Before", value=f"`{before.nick}`")
                 else:
@@ -48,27 +50,7 @@ class EventLogging(commands.Cog):
                     embed.add_field(name="After", value=f"`{before.name}`" + " (Default name)")
                 await channel.send(embed=embed)
 
-    @commands.Cog.listener()
-    async def on_member_remove(self, event: guilded.MemberRemoveEvent):
-        author = event.member
-        guild = event.server
-        kicked = event.kicked
-        banned = event.banned
-        if author.bot:
-            return
-        if banned == True:
-            channel = await guild.fetch_channel('790f5bb8-8f1c-4fa5-9ed8-518ebacf50a0')
-            em = guilded.Embed(title="A member was banned:", description="Username: `{}`\nUser ID: `{}`".format(author.name, author.id), color=0x363942)
-            await channel.send(embed=em)
-        elif kicked  == True:
-            channel = await guild.fetch_channel('790f5bb8-8f1c-4fa5-9ed8-518ebacf50a0')
-            em = guilded.Embed(title="A member was kicked:", description="Username: `{}`\nUser ID: `{}`".format(author.name, author.id), color=0x363942)
-            await channel.send(embed=em)
-        else:
-            channel = await guild.fetch_channel('790f5bb8-8f1c-4fa5-9ed8-518ebacf50a0')
-            em = guilded.Embed(title="A member has left:", description="Username: `{}`\nUser ID: `{}`".format(author.name, author.id), color=0x363942)
-            await channel.send(embed=em)
-
+    # Message Delete
     @commands.Cog.listener()
     async def on_message_delete(self, event: guilded.MessageDeleteEvent):
         message = event.message
@@ -80,32 +62,30 @@ class EventLogging(commands.Cog):
         channel = await guild.fetch_channel('790f5bb8-8f1c-4fa5-9ed8-518ebacf50a0')
         if channel:
             embed = guilded.Embed()
-            embed.title = "A message have been deleted:"
-            embed.description = f"{message.author.name} deleted a message in #{message.channel.name}:"
+            embed.title = "A Message Has Been Deleted:"
+            embed.description = f"{message.author.name} Deleted A Message In #{message.channel.name}:"
             embed.color = 0x32343D
             embed.add_field(name="Content", value=f"{message.content}")
             await channel.send(embed=embed)
-    
+
     # Joining System
-    @commands.Cog.listener()
-    async def on_member_join(self, event: guilded.MemberJoinEvent, member: guilded.Member): 
-        if event.server.default_channel_id:
-            channel = event.server.default_channel or await event.server.fetch_default_channel()
+    @commands.command()
+    async def joined(ctx, member: guilded.Member):
+        if guilded.server.default_channel_id:
+            channel = guilded.server.default_channel or await guilded.server.fetch_default_channel()
         else:
             return
-        welcome_embed = Embed(
+        welcome_embed = guilded.Embed(
             title="New Member!",
             description=f"{member.mention} has joined {member.guild.name}!",
             color=guilded.Color.green()
             )
-        """Says when a member joined."""
         await channel.send(embed=welcome_embed)
 
     # Leaving System
     @commands.Cog.listener()
-    async def on_member_remove(event: guilded.MemberRemoveEvent, member: guilded.Member):
-
-        if event.server.default_channel_id:
+    async def on_member_remove(event: guilded.MemberRemoveEvent):
+        if guilded.server.default_channel_id:
             channel = event.server.default_channel or await event.server.fetch_default_channel()
         else:
             return
@@ -119,17 +99,17 @@ class EventLogging(commands.Cog):
             cause = 'Has Been Kicked From'
             title = 'Member Kicked'
             color = guilded.Color.red()
-        else:
+        elif event.left:
             cause = 'Has Left'
             title = 'Member Left'
             color = guilded.Color.pink()
-
-        leaving_embed = Embed(
+        leaving_embed = guilded.Embed(
             title=f"{title}",
-            description=f'<@{member.name}> {cause} the server.',
+            description=f'<@{event.member.name}> {cause} the server.',
             color =f"{color}",
         )
         await channel.send(embed=leaving_embed)
-        
+
+
 def setup(bot):
     bot.add_cog(EventLogging(bot))
